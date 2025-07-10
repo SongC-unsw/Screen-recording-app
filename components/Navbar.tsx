ca582d9ccbc464e7aa44032f9843290975218415
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-07-10 11:00:30 +1000

    Add lucide-react dependency, create layout and authentication pages, and add various SVG icons

---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { LogOutIcon } from "lucide-react";
+
+const user = {};
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="flex items-center justify-between px-6 py-4 border-b border-gray-200 mb-4">
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            src={"/assets/icons/logo.svg"}
+            alt="logo"
+            width={36}
+            height={36}
+          />
+          <h1 className="text-2xl font-bold">PopCast</h1>
+        </Link>
+        {user && (
+          <figure>
+            <button className="flex items-center gap-2">
+              <Image
+                src={"/assets/images/dummy.jpg"}
+                alt="user"
+                width={36}
+                height={36}
+                className="rounded-full"
+              />
+              <button className="cursor-pointer" onClick={() => {}}>
+                <LogOutIcon className="w-5 h-5 hover:text-red-400 transition-all duration-200" />
+              </button>
+            </button>
+          </figure>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
